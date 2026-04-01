1c5e56ec7ed11f38f360d9d7faea5a4a8da76f17
Vulprit Prooze <raileyalin@gmail.com>
2026-04-01 22:25:12 +0800

    chore(infrastructure): Seeding users of all roles via appsettings

--- src/Walrhouse.Application/Common/Interfaces/ISeedUser.cs
+++
@@ -1,9 +0,0 @@
-namespace Walrhouse.Application.Common.Interfaces;
-
-public interface ISeedUser
-{
-    public string Email { get; }
-    public string Password { get; }
-    public string FirstName { get; }
-    public string LastName { get; }
-}
--- src/Walrhouse.Infrastructure/Data/ApplicationDbContextInitializer.cs
+++ src/Walrhouse.Infrastructure/Data/ApplicationDbContextInitializer.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Walrhouse.Application.Common.Interfaces;
 using Walrhouse.Domain.Constants;
 using Walrhouse.Domain.Entities;
 using Walrhouse.Infrastructure.Identity;
@@ -29,18 +31,21 @@ public class ApplicationDbContextInitializer
     private readonly ApplicationDbContext _context;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _configuration;
 
     public ApplicationDbContextInitializer(
         ILogger<ApplicationDbContextInitializer> logger,
         ApplicationDbContext context,
         UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager
+        RoleManager<IdentityRole> roleManager,
+        IConfiguration configuration
     )
     {
         _logger = logger;
         _context = context;
         _userManager = userManager;
         _roleManager = roleManager;
+        _configuration = configuration;
     }
 
     public async Task InitializeAsync()
@@ -49,9 +54,9 @@ public class ApplicationDbContextInitializer
         {
             // In Development, we wipe and recreate the database for rapid iteration
             // See https://jasontaylor.dev/ef-core-database-initialisation-strategies
-            // await _context.Database.EnsureDeletedAsync();
-            // await _context.Database.EnsureCreatedAsync();
-            _context.Database.Migrate();
+            await _context.Database.EnsureDeletedAsync();
+            await _context.Database.EnsureCreatedAsync();
+            // _context.Database.Migrate();
         }
         catch (Exception ex)
         {
@@ -76,28 +81,40 @@ public class ApplicationDbContextInitializer
     public async Task TrySeedAsync()
     {
         // Default roles
-        var administratorRole = new IdentityRole(Roles.Administrator);
-
-        if (_roleManager.Roles.All(r => r.Name != administratorRole.Name))
+        foreach (var roleName in Roles.All)
         {
-            await _roleManager.CreateAsync(administratorRole);
+            if (_roleManager.Roles.All(r => r.Name != roleName))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(roleName));
+            }
         }
 
-        // Default users
-        var administrator = new ApplicationUser
-        {
-            UserName = "administrator@localhost",
-            Email = "administrator@localhost",
-            FirstName = "System",
-            LastName = "Administrator",
-        };
+        var seedUsers = _configuration.GetSection("SeedUsers").Get<Dictionary<string, SeedUser>>();
+        Guard.Against.Null(seedUsers, message: "Configuration section 'SeedUsers' not found.");
 
-        if (_userManager.Users.All(u => u.UserName != administrator.UserName))
+        foreach (var kv in seedUsers)
         {
-            await _userManager.CreateAsync(administrator, "Administrator1!");
-            if (!string.IsNullOrWhiteSpace(administratorRole.Name))
+            var roleName = kv.Key;
+            var u = kv.Value;
+
+            if (await _userManager.FindByNameAsync(u.Email) == null)
             {
-                await _userManager.AddToRolesAsync(administrator, new[] { administratorRole.Name });
+                var user = new ApplicationUser
+                {
+                    UserName = u.Email,
+                    Email = u.Email,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                };
+
+                await _userManager.CreateAsync(user, u.Password);
+                if (
+                    !string.IsNullOrWhiteSpace(roleName)
+                    && await _roleManager.RoleExistsAsync(roleName)
+                )
+                {
+                    await _userManager.AddToRolesAsync(user, new[] { roleName });
+                }
             }
         }
 
@@ -118,4 +135,12 @@ public class ApplicationDbContextInitializer
             await _context.SaveChangesAsync();
         }
     }
+}
+
+public class SeedUser
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
 }
