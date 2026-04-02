8d2d920fc7c2a94986bbad7223e22cdd87c3dfd1
Vulprit Prooze <raileyalin@gmail.com>
2026-04-02 14:24:46 +0800

    docs(domain): docstrings to Batch entity

--- src/Walrhouse.Domain/Entities/Batch.cs
+++ src/Walrhouse.Domain/Entities/Batch.cs
@@ -2,11 +2,35 @@ namespace Walrhouse.Domain.Entities;
 
 /// <summary>
 /// Represents a batch of items.
+/// This is primarily used to track the expiration dates of items
+/// organized by batch.
 /// </summary>
 public class Batch : BaseAuditableEntity
 {
+    /// <summary>
+    /// The unique identifier of a batch.
+    /// </summary>
     public required string BatchNumber { get; set; }
 
+    /// <summary>
+    /// Gets or sets the ItemCode.
+    /// Foreign key to Item entity.
+    /// </summary>
     public required string ItemCode { get; set; }
+
+    /// <summary>
+    /// This represents the navigation property to Item entity.
+    /// </summary>
     public required Item Item { get; set; }
+
+    /// <summary>
+    /// This represents the Expiration Date of this
+    /// batch of item, measured in UTC.
+    /// </summary>
+    public required string ExpiryDate { get; set; }
+
+    /// <summary>
+    /// This represents the status of the item.
+    /// </summary>
+    public required BatchStatus Status { get; set; }
 }
