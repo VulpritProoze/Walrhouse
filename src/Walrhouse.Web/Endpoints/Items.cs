7fa8c1dcd97590aa6b3e1dc2ba8e16ed284ff555
Vulprit Prooze <raileyalin@gmail.com>
2026-04-03 21:29:24 +0800

    chore(cleanup): cleaned Items.cs Endpoint from PurgeItem

--- src/Walrhouse.Web/Endpoints/Items.cs
+++ src/Walrhouse.Web/Endpoints/Items.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Walrhouse.Application.Common.Models;
 using Walrhouse.Application.Items.Commands.CreateItem;
 using Walrhouse.Application.Items.Commands.DeleteItem;
-using Walrhouse.Application.Items.Commands.PurgeItem;
 using Walrhouse.Application.Items.Commands.UpdateItem;
 using Walrhouse.Application.Items.Queries.GetItems;
 
