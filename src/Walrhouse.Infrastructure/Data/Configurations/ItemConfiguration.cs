449ec91368c5bad0e3e58b696d7a4fa2dc965227
Vulprit Prooze <raileyalin@gmail.com>
2026-04-03 21:04:42 +0800

    chore(infrastructure): ItemConfiguration and ItemBarcodeConfiguration

--- src/Walrhouse.Infrastructure/Data/Configurations/ItemConfiguration.cs
+++ src/Walrhouse.Infrastructure/Data/Configurations/ItemConfiguration.cs
@@ -8,7 +8,18 @@ public class ItemConfiguration : IEntityTypeConfiguration<Item>
 {
     public void Configure(EntityTypeBuilder<Item> builder)
     {
-        builder.Property(i => i.ItemCode).HasMaxLength(50).IsRequired();
-        builder.Property(i => i.ItemName).HasMaxLength(200);
+        builder.Property(i => i.ItemCode).HasMaxLength(64).IsRequired();
+        builder.Property(i => i.ItemName).HasMaxLength(256);
+        builder.Property(i => i.Remarks).HasMaxLength(1024);
+        builder.Property(i => i.ItemGroup).HasMaxLength(128);
+        builder.Property(i => i.UgpEntry).HasMaxLength(64).IsRequired();
+        builder
+            .HasOne(i => i.UoMGroup)
+            .WithMany()
+            .HasForeignKey(i => i.UgpEntry)
+            .HasPrincipalKey(u => u.UgpEntry)
+            .IsRequired();
+        builder.Property(i => i.BarcodeValue).HasMaxLength(256).IsRequired();
+        builder.Property(i => i.BarcodeFormat).HasMaxLength(64);
     }
 }
