ff6f6db9f04966e94e102e49ef2fa36b15605d78
Vulprit Prooze <raileyalin@gmail.com>
2026-04-13 21:54:41 +0800

    chore(migrations): SalesOrderEntity migration

--- src/Walrhouse.Infrastructure/Migrations/20260413135241_SalesOrderEntity.Designer.cs
+++ src/Walrhouse.Infrastructure/Migrations/20260413135241_SalesOrderEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Walrhouse.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Walrhouse.Infrastructure.Data;
 namespace Walrhouse.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260413135241_SalesOrderEntity")]
+    partial class SalesOrderEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
